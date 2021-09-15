981bb40c89182fd68c0e440d8e2ad9aaf125a4ac
nataliekalantaryan <85220256+nataliekalantaryan@users.noreply.github.com>
2021-09-15 21:56:15 +0400

    Add files via upload
    Kodum mi bug ka, verjin ejum Main Page buttony chi ashxatum, chhaskaca patchary.

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function logInButton (){
+    window.location= "loginPg.html"
+}
+
+
+
+function submitButton () {
+    window.location = "quizPage.html"
+}
+
+function quizSbButton () {
+    window.location = "quizDonePage.html"
+}
+
+function mainPageButtton () {
+    window.location = "index.html"
+}
